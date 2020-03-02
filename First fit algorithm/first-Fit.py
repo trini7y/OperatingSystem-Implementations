2d90f552ff2aca317e79f84f28bbcdd71cc3efcf
trini7y <okekedesmond71@gmail.com>
2020-03-02 11:02:57 +0100

    Trying to fix index out of range issue

--- First fit algorithm/first-Fit.py
+++ First fit algorithm/first-Fit.py
@@ -7,7 +7,7 @@ busyJob = []
 frag = []
 flag = []
 allocated = []
-memLoc = []
+freeLocId = []
 
 # Creates random memomory size and locations
 def randMem(start, stop, steps):
@@ -26,7 +26,7 @@ def status(stat):
 def firstFit(memJobs, freeMem, memBlockSize):
      for i in range(len(memJobs)):
       for j in range(len(memJobs)):
-        if(flag[j] == 0 and (memJobs[i] <= memBlockSize[j]) ):
+        if(flag[j] == 0 and (memBlockSize[j] >= memJobs[i]) ):
           flag[j] = -1
           allocated[j] = 0
           busyMem.append(memBlockSize[j]);
@@ -47,10 +47,6 @@ if __name__ == "__main__":
             freeMem.append(memBlockSize[i])
             flag.append(0)
             allocated.append(-1)
-
-      # for i in range(0, ):
-      #       freeMem.remove(0)
-
       print("Mem:" , freeMem)
       print("Jobs:" , memJobs)
 
@@ -64,13 +60,19 @@ if __name__ == "__main__":
       print("allocated:" , allocated )
 
      
-      
-      print("busyJob:", busyJob)
-      print("busyJob:", busyJob) 
-    
+      for i in range(len(allocated)):
+          if(allocated[i] == -1):
+              freeLocId.append(i)
+
+      for i in range(0, freeMem.count(-1)):
+            freeMem.remove(-1)  
+
+      print("remainID:", freeLocId)
+      print("len:", len(freeLocId+identity))
+
       print("Memory location \t|", "memory BlockSize \t|", "Job Number\t|", "Job Size\t|", "Status\t|")
       for mem in range(len( memBlockSize)):
           if( flag[mem] == -1): 
-                print("\tLoc {0} \t\t\t\t| {1}K \t\t\t\t| J{2} \t\t\t\t| {3}K \t\t\t\t | {4} \t\t\t\t".format(identity[mem], memBlockSize[identity[mem]], mem+1, memJobs[mem], status(True)))
+                print("\tLoc {0} \t\t\t\t| {1}K \t\t\t\t| J{2} \t\t\t\t| {3}K \t\t\t\t| {4} \t\t\t\t".format(identity[mem], memBlockSize[ identity[mem]  ], mem+1, memJobs[mem], status(True)))
           else: 
-                print("\tLoc {0} \t\t\t\t| {1}K \t\t\t\t| J{2} \t\t\t\t| {3}K \t\t\t\t| {4}".format(identity[mem], memBlockSize[mem], mem+1, memJobs[mem], status(False) ))
+                print("\tLoc {0} \t\t\t\t| {1}K \t\t\t\t| J{2} \t\t\t\t| {3} \t\t\t\t| {4}".format(freeLocId[mem], memBlockSize[ freeLocId[mem] ], mem+1, "____", status(False) ))
